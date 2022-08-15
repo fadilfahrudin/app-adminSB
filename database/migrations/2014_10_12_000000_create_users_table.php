ee54737a44c2b951a7243fa3689477d4beaa8b3e
fadilfahrudin <fadilfahrudin32@gmail.com>
2022-08-16 00:19:36 +0700

    [update] user and table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,8 +20,15 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            
+            
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+
+
+            $table->string('no_wa');
+            $table->string('roles')->default('USER');
+
             $table->timestamps();
         });
     }
