97d9da06add05cb9df0b0cf0ed5460ede64ffd18
fadilfahrudin <fadilfahrudin32@gmail.com>
2022-08-19 20:22:09 +0700

    [update] api  program, news, user, transaction

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\API\NewsController;
+use App\Http\Controllers\API\ProgramsController;
+use App\Http\Controllers\API\TransactionController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//route group, yang harus login terlebih dahulu jika ingin menjalankan route di dalam group ini
+Route::middleware('auth:sanctum')->group(function(){
+Route::get('user', [UserController::class, 'fetch']);
+Route::post('user', [UserController::class, 'updateProfile']);
+Route::post('user/photo', [UserController::class, 'updatePhoto']);
+Route::post('logout', [UserController::class, 'logout']);
+
+Route::get('transaction', [TransactionController::class, 'all']);
+Route::post('transaction/{id}', [TransactionController::class, 'update']);
+
+
 });
+
+//noted cara bacanya:
+//Route methode ('nama route', [Nama Controller, 'nama fungsi di dalam controller']);
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+
+Route::get('program', [ProgramsController::class, 'all']);
+Route::get('news', [NewsController::class, 'all']);
