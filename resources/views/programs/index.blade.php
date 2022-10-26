a727a00ceca0c7a5877a7005ba686cef974dc36d
Fadil Fahrduin <fadilfahrudin32@gmail.com>
2022-10-26 08:20:45 +0700

    kembali ke awal

--- resources/views/programs/index.blade.php
+++ resources/views/programs/index.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="mb-10">
                 <a href="{{ route('programs.create') }}"
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Tambah Program
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">+ Tambah Program
                 </a>
             </div>
             <div class="bg-white">
