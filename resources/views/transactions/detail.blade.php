1ff9797b0e02ab52cb0e7ecb6cac750a40db7780
Fadil Fahrduin <fadilfahrudin32@gmail.com>
2022-12-15 20:38:51 +0700

    fix bug

--- resources/views/transactions/detail.blade.php
+++ resources/views/transactions/detail.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Donasi &raquo; {{ $item->program->title }} by {{ $item->user->name }}
+            Donasi &raquo; {{ $item->program->title }} by {{ $item->user_name }}
         </h2>
     </x-slot>
 
